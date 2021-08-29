4c8edfd4046905935a7cff21fe8fe7b90a198a2a
Payton Marlin <pjmddc@mail.missouri.edu>
2021-08-29 13:49:16 -0500

    Updated with picture of dice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity :
@@ -32,13 +32,53 @@ class MainActivity :
         val dice = Dice(6) //initiates instance of the Dice class
         val diceOneRoll = dice.roll() //calls the roll method, which returns a random number
         val diceTwoRoll = dice.roll()
-        val resultTextView1: TextView =
-            findViewById(R.id.dice1) //finds the view by ID and stores it in variable
-        resultTextView1.text = "Dice #1 rolled ${diceOneRoll.toString()}" //changes the text to result the dice roll
+//        val resultTextView1: TextView =
+//            findViewById(R.id.dice1) //finds the view by ID and stores it in variable
+//        resultTextView1.text = "Dice #1 rolled ${diceOneRoll.toString()}" //changes the text to result the dice roll
+//
+//        val resultTextView2: TextView =
+//            findViewById(R.id.dice2) //finds the view by ID and stores it in variable
+//        resultTextView2.text = "Dice #2 rolled ${diceTwoRoll.toString()}" //changes the text to result the dice roll
 
-        val resultTextView2: TextView =
-            findViewById(R.id.dice2) //finds the view by ID and stores it in variable
-        resultTextView2.text = "Dice #2 rolled ${diceTwoRoll.toString()}" //changes the text to result the dice roll
+        val diceImage1: ImageView = findViewById(R.id.DiceImage1)
+        val diceImage2: ImageView = findViewById(R.id.DiceImage2)
+
+        /**
+         * Instead of doing
+         * when (diceRoll) {
+        1 -> diceImage.setImageResource(R.drawable.dice_1)
+        2 -> diceImage.setImageResource(R.drawable.dice_2)
+        3 -> diceImage.setImageResource(R.drawable.dice_3)
+        4 -> diceImage.setImageResource(R.drawable.dice_4)
+        5 -> diceImage.setImageResource(R.drawable.dice_5)
+        6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
+
+         This would be better...
+         */
+        val drawableResource1 = when (diceOneRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage1.setImageResource(drawableResource1) //reduced redundancy in when clause
+        diceImage1.contentDescription = diceOneRoll.toString() //accessibility for screen readers
+
+        val drawableResource2 = when (diceTwoRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage2.setImageResource(drawableResource2) //reduced redundancy in when clause
+        diceImage2.contentDescription = diceTwoRoll.toString() //accessibility for screen readers
     }
 
 }
